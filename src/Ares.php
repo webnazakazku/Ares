47ac5c3ee0827a91eed8d225f05e2fb6dd744d7a
snk <michal.zlabek@gmail.com>
2024-01-03 18:53:42 +0100

    hotfix - new ares

--- src/Ares.php
+++ src/Ares.php
@@ -9,6 +9,22 @@ use Sunkaflek\Ares\AresRecords;
 use Sunkaflek\Ares\TaxRecord;
 use InvalidArgumentException;
 
+if (!function_exists('str_starts_with')) {
+	function str_starts_with($haystack, $needle) {
+		return (string)$needle !== '' && strncmp($haystack, $needle, strlen($needle)) === 0;
+	}
+}
+
+if (!function_exists('str_ends_with')) {
+	function str_ends_with($haystack, $needle) {
+		return $needle !== '' && substr($haystack, -strlen($needle)) === (string)$needle;
+	}
+}
+if (!function_exists('str_contains')) {
+	function str_contains($haystack, $needle) {
+		return $needle !== '' && mb_strpos($haystack, $needle) !== false;
+	}
+}
 /**
  * Class Ares.
  *
@@ -17,7 +33,7 @@ use InvalidArgumentException;
 class Ares
 {
 
-    const URL_BAS = 'http://wwwinfo.mfcr.cz/cgi-bin/ares/darv_bas.cgi?ico=%s';
+    const URL_BAS = 'https://ares.gov.cz/ekonomicke-subjekty-v-be/rest/ekonomicke-subjekty/%s';
 
     const URL_RES = 'http://wwwinfo.mfcr.cz/cgi-bin/ares/darv_res.cgi?ICO=%s';
 
@@ -129,7 +145,6 @@ class Ares
      */
     public function findByIdentificationNumber($id)
     {
-        $id = Lib::toInteger($id);
         $this->ensureIdIsInteger($id);
 
         if (empty($id)) {
@@ -148,103 +163,119 @@ class Ares
         $url = $this->wrapUrl(sprintf(self::URL_BAS, $id));
 
         try {
-            $aresRequest = file_get_contents($url, null, stream_context_create($this->contextOptions));
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+			$aresRequest = curl_exec($ch);
+			$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+			curl_close($ch);
+
+			if ($httpCode >= 400) {
+				if ($httpCode === 404) {
+					throw new AresException('IČ firmy nebylo nalezeno.');
+				}
+
+				try {
+					$aresResponse = json_decode($aresRequest, FALSE, 512, JSON_THROW_ON_ERROR);
+					throw new AresException(sprintf('Databáze ARES není dostupná. Zpráva: %s - %s - %s', $aresResponse->kod, $aresResponse->popis, $aresResponse->subKod));
+				} catch (AresException $e) {
+					throw $e;
+				} catch (\Exception $e) {
+					throw new AresException(sprintf('Databáze ARES není dostupná. Zpráva: %s', $e->getMessage()));
+				}
+			}
+
+            //$aresRequest = file_get_contents($url, false, stream_context_create($this->contextOptions));
             if ($this->debug) {
                 file_put_contents($cachedRawFile, $aresRequest);
             }
-            $aresResponse = simplexml_load_string($aresRequest);
 
+			$aresResponse = json_decode($aresRequest, FALSE, 512);
             if ($aresResponse) {
-                $ns = $aresResponse->getDocNamespaces();
-                $data = $aresResponse->children($ns['are']);
-                $elements = $data->children($ns['D'])->VBAS;
-
-                $ico = (int) $elements->ICO;
+                $ico = $aresResponse->ico ?: null;
                 if ($ico !== $id) {
                     throw new AresException('IČ firmy nebylo nalezeno.');
                 }
 
                 $record = new AresRecord();
 
-                $record->setCompanyId(strval($elements->ICO));
-                $record->setTaxId(strval($elements->DIC));
-
-                //prevent company names in quotes
-                if (substr(strval($elements->OF), 0, 1) === '"' AND substr(strval($elements->OF), -1) ==='"') {
-                    $record->setCompanyName(substr(strval($elements->OF),1,-1));
-                } else {
-                    $record->setCompanyName(strval($elements->OF));
-                }
+                $record->setCompanyId($ico);
+                $record->setTaxId($aresResponse->dic ?? '');
                 
-                if (strval($elements->AA->NU)) {
-                    $record->setStreet(strval($elements->AA->NU));
-                //Put town or part of town as street if there is no street to prevent just house number as street
-                } else if (strval($elements->AA->NCO)) {
-                    $record->setStreet(strval($elements->AA->NCO)); 
-                } else if (strval($elements->AA->N)) {
-                    $record->setStreet(strval($elements->AA->N)); 
+                $obchodniJmeno = $aresResponse->obchodniJmeno;
+                if (substr($obchodniJmeno, 0, 1) === '"' AND substr($obchodniJmeno, -1) ==='"') {
+                    $obchodniJmeno = substr($obchodniJmeno,1,-1);
                 }
-
-                if (strval($elements->AA->CO)) {
-                    $record->setStreetHouseNumber(strval($elements->AA->CD));
-                    $record->setStreetOrientationNumber(strval($elements->AA->CO));
-                } else if (strval($elements->AA->CD)){
-                    $record->setStreetHouseNumber(strval($elements->AA->CD));
-                } else {
-                    $record->setStreetHouseNumber(strval($elements->AA->CA));
-                }
-
-                //Prevents doubling town area (like "Praha-Libuš - Libuš" or "Brandýs nad Labem-Stará Boleslav - Brandýs nad Labem"
-                $townAlreadyContainsArea = false;
-                if (
-                    strval($elements->AA->N) === 'Praha' 
-                    AND !empty(strval($elements->AA->NCO)) 
-                    AND strpos(strval($elements->AA->NMC), strval($elements->AA->NCO)) !== false
-                   ) 
-                {
-                    $townAlreadyContainsArea = true;
-                } else if (
-                    !empty(strval($elements->AA->NCO)) 
-                    AND strpos(strval($elements->AA->N), strval($elements->AA->NCO)) !== false
-                ) 
-                {
-                    $townAlreadyContainsArea = true;
-                }
-
-                if (strval($elements->AA->N) === 'Praha') { //Praha
-
-                    //If Praha is not mentioned in NMC, which happens, albeit rarely. Whithout this the town result would look like " - Vinohrady"
-                    if (strpos(strval($elements->AA->NMC), 'Praha') === false) {
-                        $record->setTown(strval($elements->AA->N) . ' - ' . strval($elements->AA->NCO));
-                    } else {
-                        if (!$townAlreadyContainsArea) {
-                            $record->setTown(strval($elements->AA->NMC) . ' - ' . strval($elements->AA->NCO));
-                        } else {
-                            $record->setTown(strval($elements->AA->NMC));
-                        }
-                    }
                     
-                } elseif (
-                    !empty(strval($elements->AA->NCO)) 
-                    AND strval($elements->AA->NCO) !== strval($elements->AA->N) 
-                    AND !$townAlreadyContainsArea
-                    ) 
-                { //Ostrava
-                    //Prevents duplication in town like "České Budějovice - České Budějovice 3"
-                    $areaBeginsWithTown = false;
-                    if (substr(strval($elements->AA->NCO), 0, strlen(strval($elements->AA->N))) === strval($elements->AA->N)) {
-                        $areaBeginsWithTown = true;
-                    }
-                    if (!$areaBeginsWithTown) {
-                        $record->setTown(strval($elements->AA->N) . ' - ' . strval($elements->AA->NCO));
-                    } else {
-                        $record->setTown(strval($elements->AA->NCO));
-                    }
-                } else {
-                    $record->setTown(strval($elements->AA->N));
-                }
+				$record->setCompanyName($obchodniJmeno);
 
-                $record->setZip(strval($elements->AA->PSC));
+				$street = $aresResponse->sidlo->nazevUlice ?? '';
+				if ($street === '') {
+					$street = $aresResponse->sidlo->nazevCastiObce ?? '';
+				}
+
+				$record->setStreet($street);
+				$record->setStreetHouseNumber($aresResponse->sidlo->cisloDomovni ?? '');
+                
+                $cisloOrientacni = $aresResponse->sidlo->cisloOrientacni ?? '';
+                if (isset($aresResponse->sidlo->cisloOrientacniPismeno)) $cisloOrientacni .= $aresResponse->sidlo->cisloOrientacniPismeno;
+                
+				$record->setStreetOrientationNumber($cisloOrientacni);
+				$town = $aresResponse->sidlo->nazevObce ?? '';
+
+				if ($town === 'Praha') {
+					if (isset($aresResponse->sidlo->nazevMestskeCastiObvodu)) {
+						$town = $aresResponse->sidlo->nazevMestskeCastiObvodu;
+						if (str_contains($town, '-')) {
+							$town = $aresResponse->sidlo->nazevMestskehoObvodu ?? $town;
+						}
+
+						$townPart = $aresResponse->sidlo->nazevCastiObce ?? NULL;
+						if ($townPart !== NULL && $townPart !== $town) {
+							$town .= ' - ' . $townPart;
+						}
+					} else {
+						$town = $aresResponse->sidlo->nazevMestskehoObvodu ?? $town;
+					}
+				} else {
+					$townPart = $aresResponse->sidlo->nazevCastiObce ?? NULL;
+					if ($townPart !== NULL && $townPart !== $town) {
+						$townPartContainsTown = FALSE;
+						if (str_starts_with($townPart, $town)) {
+							$townPartContainsTown = TRUE;
+						}
+
+						if ($townPartContainsTown) {
+							$town = $townPart;
+						} else {
+							$town .= ' - ' . $townPart;
+						}
+					}
+				}
+
+				$record->setTown($town);
+				$record->setZip((string)($aresResponse->sidlo->psc ?? ''));
+
+				if (isset($aresResponse->sidlo->textovaAdresa) && $record->getZip() === '' && $record->getTown() === '' && trim($record->getStreet()) === '') {
+					$re = '/([a-zA-ZěščřžýáíéĚŠČŘŽÝÁÍÉ ]+), (.*), PSČ ([0-9 ]+), /mUu';
+					preg_match($re, $aresResponse->sidlo->textovaAdresa, $matches);
+
+					if ($matches !== []) {
+						$record->setStreet($matches[2]);
+						$record->setTown($matches[1]);
+						$record->setZip(str_replace(' ', '', $matches[3]));
+					} else {
+						$re = '/([0-9 ]+) ([a-zA-ZěščřžýáíéĚŠČŘŽÝÁÍÉ 0-9-]+), /mUu';
+						preg_match($re, $aresResponse->sidlo->textovaAdresa, $matches);
+
+						if ($matches !== []) {
+							$record->setStreet($matches[2]);
+							$record->setTown($matches[2]);
+							$record->setZip(str_replace(' ', '', $matches[1]));
+						}
+					}
+				}
             } else {
                 throw new AresException('Databáze ARES není dostupná.');
             }
@@ -454,7 +485,7 @@ class Ares
      */
     private function ensureIdIsInteger($id)
     {
-        if (!is_int($id)) {
+        if (!is_numeric($id)) {
             throw new InvalidArgumentException('IČ firmy musí být číslo.');
         }
     }
